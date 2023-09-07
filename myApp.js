5bf84381f13d6181f2e1a25c850121ee7f1cfe0a
Andrew <leducanh017@gmail.com>
2023-09-07 17:28:31 +0300

    first commit

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3030;  
 
 app.listen(PORT, () => {
   
