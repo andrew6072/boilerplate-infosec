d7c8ed3142142eb3994555f4caa15981a0f2e44a
Andrew <leducanh017@gmail.com>
2023-09-07 23:06:10 +0300

    fix myApp

--- myApp.js
+++ myApp.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const app = express();
-
-const PORT = process.env.PORT || 3030;  
+const PORT = process.env.PORT || 3030;
 
 app.listen(PORT, () => {
   
